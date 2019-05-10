a6be7f0fc1ebf3fd60ac2e9b2bb8dee9c7bcd6b4
CrazyVito11 <31567466+CrazyVito11@users.noreply.github.com>
2019-05-10 12:12:51 +0200

    Update inschrijvingen.sql

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,7 @@
 USE lo8e_sql2;
 
-INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES 
-	('lo9e-amo1','vlt03', 2019, 'amo'), 
-	('lo9e-amo2','brc01', 2019, 'amo'), 
-    ('lo9e-amo3','rs001', 2019, 'amo');
+INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboortedatum, postcode, plaats, gewicht, lengte, inschrijvings_datum) VALUES
+	(90000,'Choo','','Jonny','1993-07-27','2211 SG','Noordwijkerhout', 73.2, 1.86, 01-01-2019),
+	(90001,'Bakker','','Klaas','1992-08-05','2152 EX','Nieuw-Vennep', 82.1, 1.87, 01-01-2019),
+	(90002,'Wilders','','Geert','1994-03-26','2465 AG','Rijsaterwoude', 67.0, 1.77, 01-01-2019),
+    (90003,'Herbert','','Dean ','1992-08-05','2152 EX','Nieuw-Vennep', 82.1, 1.87, 01-01-2019);
